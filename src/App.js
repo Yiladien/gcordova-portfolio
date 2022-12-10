a9f92144bba599b5bdae5181995ed3bedd7e25ff
yiladien@hotmail.com <yiladien@hotmail.com>
2022-12-10 15:52:13 -0600

    navigation state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "bulma/css/bulma.min.css";
 import "./index.css";
 import About from "./components/About";
@@ -8,10 +9,19 @@ import Portfolio from "./components/Portfolio";
 import Resume from "./components/Resume";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("about");
+
+  console.log(currentPage);
+
   return (
     <div>
-      <Nav />
-      <Resume />
+      <Nav setCurrentPage={setCurrentPage}></Nav>
+      <main>
+        {currentPage === "about" && <About></About>}
+        {currentPage === "portfolio" && <Portfolio></Portfolio>}
+        {currentPage === "resume" && <Resume></Resume>}
+        {currentPage === "contact" && <Contact></Contact>}
+      </main>
       <Footer />
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,32 +1,60 @@
 import React from "react";
 import logo from "../../assets/small/gc-logo-0.5x.png";
 
-const Nav = () => {
+function Nav(props) {
+  const { setCurrentPage } = props;
+
   return (
     <div>
       <nav className="level is-mobile py-2 is-fixed-top">
         <p className="level-item"></p>
         <p className="level-item has-text-centered">
-          <a className="has-text-white">About</a>
+          <a
+            className="has-text-white"
+            href="#about"
+            onClick={() => setCurrentPage("about")}
+          >
+            About
+          </a>
         </p>
         <p className="level-item has-text-centered">
-          <a className="has-text-white">Portfolio</a>
+          <a
+            className="has-text-white"
+            href="#portfolio"
+            onClick={() => setCurrentPage("portfolio")}
+          >
+            Portfolio
+          </a>
         </p>
         <div className="level-item has-text-centered">
-          <figure className="image is-64x64">
-            <img src={logo} alt="" height="50"></img>
-          </figure>
+          <a href="#about" onClick={() => setCurrentPage("about")}>
+            <figure className="image is-64x64">
+              <img src={logo} alt="" height="50"></img>
+            </figure>
+          </a>
         </div>
         <p className="level-item has-text-centered">
-          <a className="has-text-white">Resume</a>
+          <a
+            className="has-text-white"
+            href="#resume"
+            onClick={() => setCurrentPage("resume")}
+          >
+            Resume
+          </a>
         </p>
         <p className="level-item has-text-centered">
-          <a className="has-text-white">Contact</a>
+          <a
+            className="has-text-white"
+            href="#contact"
+            onClick={() => setCurrentPage("contact")}
+          >
+            Contact
+          </a>
         </p>
         <p className="level-item"></p>
       </nav>
     </div>
   );
-};
+}
 
 export default Nav;
